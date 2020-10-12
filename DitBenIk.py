3f68ebebf390e06db09a704253a88e54ccfa143d
Olli Appelt <30849@ma-web.nl>
2020-10-12 10:02:07 +0100

    DitBenIk.py

---
+++ DitBenIk.py
@@ -0,0 +1,74 @@
+print("Hello you!, ik ben Olli")
+print("Wie ben jij?")
+
+voornaam = input()
+
+print("Hello" + " " + voornaam)
+print("Ik ben een nieuwkomer op het Mediacollege Amsterdam. Door een aantal vragen over mij te beantwoorden leer je mij beter kennen.")
+print("Ik woon in?")
+print("A = Noordwijk")
+print("B = Amsterdam")
+print("C = Zaandam")
+print("Kies een antwoord: A, B of C >")
+
+keuze = input()
+
+if keuze == 'A':
+    print("Dat is juist!")
+elif keuze == 'B':
+    print("Nee, een stukje verder weg!")
+elif keuze == 'C':
+    print("Nee, een stukje verder weg!")
+else:
+    print(keuze, " is geen antwoord!")
+
+print("--------------------------------------------------------------")
+
+print("Waar heb ik op de middelbare gezeten?")
+print("A = Noordwijk")
+print("B = Katwijk")
+print("C = Noordwijkerhout")
+print("Kies een antwoord: A, B of C >")
+
+keuze = input()
+
+if keuze == 'A':
+    print("Dat is juist! Ik ga niet verder dan mijn eigen dorp fietsen!")
+elif keuze == 'B':
+    print("Nee, Dat is wel een stukje fietsen!")
+elif keuze == 'C':
+    print("Nee, Dat is wel een stukje fietsen!")
+else:
+    print(keuze, " is geen antwoord!")
+
+print("--------------------------------------------------------------")
+
+print("Met welk voertuig rijd ik altijd naar school?")
+print("A = Trein & Bus")
+print("B = Vliegtuig & Trein")
+print("C = Auto & Bus")
+print("Kies een antwoord: A, B of C >")
+
+keuze = input()
+
+if keuze == 'A':
+    print("Jusit! Nu weet je meer over mij!")
+elif keuze == 'B':
+    print("Nee, Hoe?")
+elif keuze == 'C':
+    print("Nee, Was het maar zo'n feest!")
+else:
+    print(keuze, " is geen antwoord!")
+
+
+
+
+
+
+
+
+
+    
+
+
+
